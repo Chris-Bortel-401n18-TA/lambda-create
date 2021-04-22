32be6b947e56c91be559d2839bf7d8b1c5d43a45
Chris-Bortel <bortelcc19@gmail.com>
2021-04-22 15:42:29 -0700

    The commit setsup the basic scaffold for our lambda function

---
+++ index.js
@@ -0,0 +1,33 @@
+'use strict'
+
+const uuid = require('uuid').v4;
+const peopleModel = require('./schema.js');
+
+// AWS lambda functions export a handler that is an async function that takes in an event
+exports.handler = async (event) => {
+  console.log(event);
+
+  try {
+    // NOTE: if you cannot parse the thing that is being recieved, we will recieve an error of cannot parse O of undefined so we do a try catch
+    const body = JSON.parse(event.body);
+    const { name, occupation } = body;
+    const id = uuid();
+
+    const record = new peopleModel({ id, name, occupation });
+    const data = await record.save();
+    const savedRecord = JSON.stringify(data);
+    
+    return {
+      statusCode: 201,
+      response: savedRecord,
+    };
+
+  } catch {
+    return {
+      statusCode: 500,
+      response: exports.message,
+    }
+    
+  }
+    
+}
